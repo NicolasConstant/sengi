f2ef6544a54d1dc4c9c7cf3e89e01aec70868352
Nicolas Constant <github@nicolas-constant.com>
2018-09-26 00:53:09 -0400

    added Ctrl+Enter keybind

--- src/app/components/floating-column/add-new-status/add-new-status.component.ts
+++ src/app/components/floating-column/add-new-status/add-new-status.component.ts
@@ -67,4 +67,9 @@ export class AddNewStatusComponent implements OnInit {
         var regAccounts = <AccountInfo[]>this.store.snapshot().registeredaccounts.accounts;
         return regAccounts;
     }
+
+    onCtrlEnter(): boolean {
+        this.onSubmit();
+        return false;
+    }
 }
