82b97baf4f93b442ef04d0c4857c02353f199b69
Nicolas Constant <github@nicolas-constant.com>
2018-10-28 19:45:18 -0400

    added JUnit export

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,12 +21,24 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+
+    // the default configuration
+    junitReporter: {
+        outputDir: '', // results will be saved as $outputDir/$browserName.xml
+        outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+        suite: '', // suite will become the package name attribute in xml testsuite element
+        useBrowserName: true, // add browser name to report and classes names
+        nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+        classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+        properties: {}, // key value pair of properties to add to the <properties> section of the report
+        xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+      }
   });
 };
