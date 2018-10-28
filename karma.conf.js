3f82851b778c52a65e6de4127d9c93a552b3bee7
Nicolas Constant <github@nicolas-constant.com>
2018-10-28 19:53:44 -0400

    trigger ci

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'), 
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
+      require('karma-junit-reporter') 
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
