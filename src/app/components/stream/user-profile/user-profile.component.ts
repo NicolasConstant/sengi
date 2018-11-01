c3fedcd1de083b8c5c37fc96850d31f85073fc54
Nicolas Constant <github@nicolas-constant.com>
2018-11-01 00:44:58 -0400

    now loading status in user profile

--- src/app/components/stream/user-profile/user-profile.component.ts
+++ src/app/components/stream/user-profile/user-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Account } from "../../../services/models/mastodon.interfaces";
+import { Account, Status } from "../../../services/models/mastodon.interfaces";
+import { MastodonService } from '../../../services/mastodon.service';
+import { ToolsService } from '../../../services/tools.service';
+import { StatusWrapper } from '../stream.component';
 
 @Component({
     selector: 'app-user-profile',
@@ -10,6 +13,9 @@ export class UserProfileComponent implements OnInit {
     account: Account;
     hasNote: boolean;
 
+    statusLoading: boolean;
+    statuses: StatusWrapper[] =  [];
+
     @Output() browseAccount = new EventEmitter<string>();
     @Output() browseHashtag = new EventEmitter<string>();
 
@@ -19,9 +25,12 @@ export class UserProfileComponent implements OnInit {
         this.hasNote = account && account.note && account.note !== '<p></p>';
         console.warn('currentAccount');
         console.warn(account);
+        this.getStatuses(account);
     }
 
-    constructor() { }
+    constructor(
+        private readonly mastodonService: MastodonService,
+        private readonly toolsService: ToolsService) { }
 
     ngOnInit() {
     }
@@ -32,5 +41,25 @@ export class UserProfileComponent implements OnInit {
 
     hashtagSelected(hashtag: string): void {
         this.browseHashtag.next(hashtag);
+    }
+
+    private getStatuses(account: Account): void {
+        let selectedAccounts = this.toolsService.getSelectedAccounts();
+        if (selectedAccounts.length === 0) return;
+
+        this.statusLoading = true;
+        this.mastodonService.getAccountStatuses(selectedAccounts[0], account.id, false, false, true, null, null, 40)
+            .then((result: Status[]) => {
+                for (const status of result) {
+                    const wrapper = new StatusWrapper(status,selectedAccounts[0]);
+                    this.statuses.push(wrapper);
+                }
+            })
+            .catch(err => {
+
+            })
+            .then(() => {
+                this.statusLoading = false;
+            });
     }
 }
