bb62c8262b99fd5db29f55371763744e7a93ed31
Nicolas Constant <github@nicolas-constant.com>
2018-11-02 23:36:35 -0400

    trying a new way to display account

--- src/app/components/stream/user-profile/user-profile.component.ts
+++ src/app/components/stream/user-profile/user-profile.component.ts
@@ -33,20 +33,15 @@ export class UserProfileComponent implements OnInit {
         this.loadAccount(accountName)
             .then((account: Account) => {
                 this.account = account;
+                this.hasNote = account && account.note && account.note !== '<p></p>';
                 return this.getStatuses(this.account);
             })
             .catch(err => {
                 this.error = 'Error when retrieving account';
                 this.isLoading = false;
                 this.statusLoading = false;
-                console.warn(this.error);
+                console.error(this.error);
             });
-
-        // this.account = account;
-        // this.hasNote = account && account.note && account.note !== '<p></p>';
-        // console.warn('currentAccount');
-        // console.warn(account);
-        // this.getStatuses(account);
     }
 
     constructor(
