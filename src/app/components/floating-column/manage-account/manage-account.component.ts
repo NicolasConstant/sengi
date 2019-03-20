d99976f0a7bef45f3c5dc4dd57258fafcd96b5d7
Nicolas Constant <github@nicolas-constant.com>
2019-03-19 23:13:50 -0400

    migration of my-account to its component

--- src/app/components/floating-column/manage-account/manage-account.component.ts
+++ src/app/components/floating-column/manage-account/manage-account.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Store } from '@ngxs/store';
 import { faAt, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { faBell, faEnvelope, faUser, faStar } from "@fortawesome/free-regular-svg-icons";
 
-import { StreamElement, StreamTypeEnum, AddStream, RemoveAllStreams } from '../../../states/streams.state';
-import { RemoveAccount } from '../../../states/accounts.state';
 import { AccountWrapper } from '../../../models/account.models';
-import { NavigationService } from '../../../services/navigation.service';
-import { NotificationService } from '../../../services/notification.service';
 
 @Component({
     selector: 'app-manage-account',
@@ -25,39 +20,12 @@ export class ManageAccountComponent implements OnInit {
     subPanel = 'account';
     hasNotifications = false;
     hasMentions = false;
-
+    
     @Input() account: AccountWrapper;
 
-    availableStreams: StreamElement[] = [];
+    constructor() { }
 
-    constructor(
-        private readonly store: Store,
-        private readonly navigationService: NavigationService,
-        private notificationService: NotificationService) { }
-
-    ngOnInit() {
-        const instance = this.account.info.instance;
-        this.availableStreams.length = 0;
-        this.availableStreams.push(new StreamElement(StreamTypeEnum.global, 'Federated Timeline', this.account.info.id, null, null, instance));
-        this.availableStreams.push(new StreamElement(StreamTypeEnum.local, 'Local Timeline', this.account.info.id, null, null, instance));
-        this.availableStreams.push(new StreamElement(StreamTypeEnum.personnal, 'Home', this.account.info.id, null, null, instance));
-    }
-
-    addStream(stream: StreamElement): boolean {
-        if (stream) {
-            this.store.dispatch([new AddStream(stream)]).toPromise()
-                .then(() => {
-                    this.notificationService.notify(`stream added`, false);
-                });
-        }
-        return false;
-    }
-
-    removeAccount(): boolean {
-        const accountId = this.account.info.id;
-        this.store.dispatch([new RemoveAllStreams(accountId), new RemoveAccount(accountId)]);
-        this.navigationService.closePanel();
-        return false;
+    ngOnInit() {       
     }
 
     loadSubPanel(subpanel: string): boolean {
--- src/app/components/floating-column/manage-account/my-account/my-account.component.ts
+++ src/app/components/floating-column/manage-account/my-account/my-account.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Store } from '@ngxs/store';
+
+import { NotificationService } from '../../../../services/notification.service';
+import { StreamElement, StreamTypeEnum, AddStream, RemoveAllStreams } from '../../../../states/streams.state';
+import { AccountWrapper } from '../../../../models/account.models';
+import { RemoveAccount } from '../../../../states/accounts.state';
+import { NavigationService } from '../../../../services/navigation.service';
 
 @Component({
-  selector: 'app-my-account',
-  templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+    selector: 'app-my-account',
+    templateUrl: './my-account.component.html',
+    styleUrls: ['./my-account.component.scss']
 })
 export class MyAccountComponent implements OnInit {
+    
+    availableStreams: StreamElement[] = [];
 
-  constructor() { }
+    @Input() account: AccountWrapper;
 
-  ngOnInit() {
-  }
+    constructor(
+        private readonly store: Store,
+        private readonly navigationService: NavigationService,
+        private notificationService: NotificationService) { }
 
+    ngOnInit() {
+        const instance = this.account.info.instance;
+        this.availableStreams.length = 0;
+        this.availableStreams.push(new StreamElement(StreamTypeEnum.global, 'Federated Timeline', this.account.info.id, null, null, instance));
+        this.availableStreams.push(new StreamElement(StreamTypeEnum.local, 'Local Timeline', this.account.info.id, null, null, instance));
+        this.availableStreams.push(new StreamElement(StreamTypeEnum.personnal, 'Home', this.account.info.id, null, null, instance));
+    }
+
+    addStream(stream: StreamElement): boolean {
+        if (stream) {
+            this.store.dispatch([new AddStream(stream)]).toPromise()
+                .then(() => {
+                    this.notificationService.notify(`stream added`, false);
+                });
+        }
+        return false;
+    }
+
+    removeAccount(): boolean {
+        const accountId = this.account.info.id;
+        this.store.dispatch([new RemoveAllStreams(accountId), new RemoveAccount(accountId)]);
+        this.navigationService.closePanel();
+        return false;
+    }
 }
