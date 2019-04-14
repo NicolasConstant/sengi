8ecbd53b5b5ccf72dae8f4260e897bfd8b40b0f9
Nicolas Constant <github@nicolas-constant.com>
2019-04-13 23:04:19 -0400

    first iteration of emoji support #47

--- src/app/components/stream/status/status.component.ts
+++ src/app/components/stream/status/status.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from "@angu
 import { faStar, faRetweet } from "@fortawesome/free-solid-svg-icons";
 
 import { Status, Account } from "../../../services/models/mastodon.interfaces";
-import { OpenThreadEvent } from "../../../services/tools.service";
+import { OpenThreadEvent, ToolsService } from "../../../services/tools.service";
 import { ActionBarComponent } from "./action-bar/action-bar.component";
 import { StatusWrapper } from '../../../models/common.model';
+import { EmojiConverter, EmojiTypeEnum } from '../../../tools/emoji.tools';
 
 @Component({
     selector: "app-status",
@@ -12,10 +13,16 @@ import { StatusWrapper } from '../../../models/common.model';
     styleUrls: ["./status.component.scss"]
 })
 export class StatusComponent implements OnInit {
+    private emojiConverter = new EmojiConverter();
+
     faStar = faStar;
     faRetweet = faRetweet;
 
     displayedStatus: Status;
+
+    statusAccountName: string;
+    statusContent: string;
+
     reblog: boolean;
     hasAttachments: boolean;
     replyingToStatus: boolean;
@@ -59,6 +66,12 @@ export class StatusComponent implements OnInit {
         if (this.displayedStatus.media_attachments && this.displayedStatus.media_attachments.length > 0) {
             this.hasAttachments = true;
         }
+
+
+        // const instanceUrl = 'https://' + this.status.uri.split('https://')[1].split('/')[0];
+
+        this.statusAccountName = this.emojiConverter.applyEmojis(this.displayedStatus.account.emojis, this.displayedStatus.account.display_name, EmojiTypeEnum.small,  this.status.uri);
+        this.statusContent = this.emojiConverter.applyEmojis(this.displayedStatus.emojis, this.displayedStatus.content, EmojiTypeEnum.medium,  this.status.uri);
     }
     get statusWrapper(): StatusWrapper {
         return this._statusWrapper;
