106a0191c5b9dac27d9b18b0c944932284824f75
Nicolas Constant <github@nicolas-constant.com>
2019-04-17 23:25:54 -0400

    added emojis as local assets

--- src/app/tools/emoji.tools.ts
+++ src/app/tools/emoji.tools.ts
@@ -3,7 +3,7 @@ import * as EmojiOne from 'emojione';
 
 export class EmojiConverter {
     applyEmojis(emojis: Emoji[], text: string, type: EmojiTypeEnum, url: string): string {
-        const instanceUrl = 'https://' + url.split('https://')[1].split('/')[0];
+        //const instanceUrl = 'https://' + url.split('https://')[1].split('/')[0];
 
         let className = 'emoji-small';
         if (type === EmojiTypeEnum.medium) {
@@ -17,12 +17,17 @@ export class EmojiConverter {
         text = EmojiOne.toImage(text);
 
         while (text.includes('class="emojione"')) {
-            text = text.replace('class="emojione"', `class="emojione ${className}" onerror="this.style.display='none'"`);
+            text = text.replace('class="emojione"', `class="emojione ${className}"`);
         }
 
         //FIXME: clean up this mess...
+        // while (text.includes('https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/')) {
+        //     text = text.replace('https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/', instanceUrl + '/emoji/');
+        //     text = text.replace('.png', '.svg');
+        // }
+
         while (text.includes('https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/')) {
-            text = text.replace('https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/', instanceUrl + '/emoji/');
+            text = text.replace('https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/', 'assets/emoji/');
             text = text.replace('.png', '.svg');
         }
 
