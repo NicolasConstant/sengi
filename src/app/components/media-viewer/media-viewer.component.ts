0136d71e2b4efc07a544901278a990be87bf96ff
Nicolas Constant <github@nicolas-constant.com>
2019-05-17 21:13:23 -0400

    image navigation functionnal #92

--- src/app/components/media-viewer/media-viewer.component.ts
+++ src/app/components/media-viewer/media-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, Output, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 import { faTimes, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { Subject } from 'rxjs';
@@ -12,7 +12,7 @@ import { Attachment } from '../../services/models/mastodon.interfaces';
     templateUrl: './media-viewer.component.html',
     styleUrls: ['./media-viewer.component.scss']
 })
-export class MediaViewerComponent implements OnInit {   
+export class MediaViewerComponent implements OnInit {
     private _mediaEvent: OpenMediaEvent;
     faTimes = faTimes;
     faAngleLeft = faAngleLeft;
@@ -23,6 +23,10 @@ export class MediaViewerComponent implements OnInit {
     videoUrl: string;
     html: SafeHtml;
 
+    previousAvailable: boolean;
+    nextAvailable: boolean;
+    private currentIndex: number;
+
     @Input('openedMediaEvent')
     set openedMediaEvent(value: OpenMediaEvent) {
         this._mediaEvent = value;
@@ -32,7 +36,9 @@ export class MediaViewerComponent implements OnInit {
             this.autoplayIframe();
         } else {
             const attachment = value.attachments[value.selectedIndex];
+            this.currentIndex = value.selectedIndex;
             this.loadAttachment(attachment);
+            this.setBrowsing();
         }
     }
     get openedMediaEvent(): OpenMediaEvent {
@@ -41,6 +47,18 @@ export class MediaViewerComponent implements OnInit {
 
     @Output() closeSubject = new Subject<boolean>();
     @ViewChild('video') myVideo: ElementRef;
+
+    @HostListener('document:keydown', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        event.stopPropagation();
+        event.preventDefault();
+        
+        if (event.key === 'ArrowRight') {
+            this.next(event);
+        } else if (event.key === 'ArrowLeft') {
+            this.previous(event);
+        }
+    }
 
     constructor() { }
 
@@ -57,12 +75,32 @@ export class MediaViewerComponent implements OnInit {
         }
     }
 
+    private setBrowsing() {
+        var index = this.currentIndex;
+        var attachments = this.openedMediaEvent.attachments;
+
+        console.log(`index ${index}`);
+        console.log(`attachments.length ${attachments.length}`);
+
+        if (index < attachments.length - 1) {
+            this.nextAvailable = true;
+        } else {
+            this.nextAvailable = false;
+        }
+
+        if (index > 0) {
+            this.previousAvailable = true;
+        } else {
+            this.previousAvailable = false;
+        }
+    }
+
     private autoplayIframe(): any {
         setTimeout(() => {
-            if(this.myVideo.nativeElement.childNodes[0].src.includes('?')){
-                this.myVideo.nativeElement.childNodes[0].src+='&autoplay=1&auto_play=1';
+            if (this.myVideo.nativeElement.childNodes[0].src.includes('?')) {
+                this.myVideo.nativeElement.childNodes[0].src += '&autoplay=1&auto_play=1';
             } else {
-                this.myVideo.nativeElement.childNodes[0].src+='?autoplay=1&auto_play=1';
+                this.myVideo.nativeElement.childNodes[0].src += '?autoplay=1&auto_play=1';
             }
         }, 500);
     }
@@ -77,14 +115,24 @@ export class MediaViewerComponent implements OnInit {
         return false;
     }
 
-    previous(event): boolean{
+    previous(event): boolean {
         event.stopPropagation();
+        if (this.currentIndex <= 0) return false;
+
+        this.currentIndex--;
+        this.imageUrl = this.openedMediaEvent.attachments[this.currentIndex].url;
+        this.setBrowsing();
 
         return false;
     }
 
-    next(event): boolean{
+    next(event): boolean {
         event.stopPropagation();
+        if (this.currentIndex >= this.openedMediaEvent.attachments.length - 1) return false;
+
+        this.currentIndex++;
+        this.imageUrl = this.openedMediaEvent.attachments[this.currentIndex].url;
+        this.setBrowsing();
 
         return false;
     }
