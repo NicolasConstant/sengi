5ed1e562e4c8db04a282fd7257022e1dbd816a1f
Nicolas Constant <github@nicolas-constant.com>
2019-06-01 14:05:03 -0400

    fix strange memory debuging issue

--- src/app/tools/emoji.tools.ts
+++ src/app/tools/emoji.tools.ts
@@ -1,8 +1,9 @@
 import { Emoji } from "../services/models/mastodon.interfaces";
-import { EmojiOne } from "./emoji-one";
+// import { EmojiOne } from "./emoji-one";
+import * as EmojiOne from 'emojione';
 
 export class EmojiConverter {
-    private emojiOne = new EmojiOne();
+    // private emojiOne = new EmojiOne();
 
     applyEmojis(emojis: Emoji[], text: string, type: EmojiTypeEnum): string {
         if (!text) return text;
@@ -14,12 +15,38 @@ export class EmojiConverter {
 
         if (emojis) {
             emojis.forEach(emoji => {
-                text = this.replaceAll(text, `:${emoji.shortcode}:`,  `<img class="${className}" src="${emoji.url}" title=":${
-                    emoji.shortcode }:" alt=":${emoji.shortcode}:" />`)
+                try {
+                    text = this.replaceAll(text, `:${emoji.shortcode}:`, `<img class="${className}" src="${emoji.url}" title=":${
+                        emoji.shortcode}:" alt=":${emoji.shortcode}:" />`);
+                } catch (err) {}
             });
         }
 
-        text = this.emojiOne.toImage(text, className);
+        text = this.applyEmojiOne(className, text);
+
+        // try {
+        //     text = this.emojiOne.toImage(text, className);
+        // } catch (err) {}
+        return text;
+    }
+
+    private applyEmojiOne(className: string, text: string): string{
+        text = EmojiOne.toImage(text);
+
+        while (text.includes('class="emojione"')) {
+          text = text.replace('class="emojione"', `class="emojione ${className}"`);
+        }
+    
+        while (
+          text.includes("https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/")
+        ) {
+          text = text.replace(
+            "https://cdn.jsdelivr.net/emojione/assets/4.5/png/32/",
+            "assets/emoji/72x72/"
+          );
+          // text = text.replace('.png', '.svg');
+        }
+
         return text;
     }
 
