f96309ed01ec2cffb8a62150bf5de3ab7de3e5bb
Nicolas Constant <github@nicolas-constant.com>
2019-09-06 20:35:42 -0400

    better display of scheduled statuses

--- src/app/components/floating-column/scheduled-statuses/scheduled-statuses.component.ts
+++ src/app/components/floating-column/scheduled-statuses/scheduled-statuses.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { ScheduledStatusService, ScheduledStatusNotification } from '../../../services/scheduled-status.service';
+import { ScheduledStatus } from '../../../services/models/mastodon.interfaces';
+import { AccountInfo } from '../../../states/accounts.state';
 
 @Component({
     selector: 'app-scheduled-statuses',
@@ -10,7 +12,8 @@ import { ScheduledStatusService, ScheduledStatusNotification } from '../../../se
 })
 export class ScheduledStatusesComponent implements OnInit, OnDestroy {
     private statusSub: Subscription;
-    scheduledStatuses: ScheduledStatusNotification[] = [];
+    // scheduledStatuses: ScheduledStatusNotification[] = [];
+    scheduledStatuses: ScheduledStatusWrapper[] = [];
 
     constructor(
         private readonly scheduledStatusService: ScheduledStatusService) {            
@@ -18,12 +21,27 @@ export class ScheduledStatusesComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.statusSub = this.scheduledStatusService.scheduledStatuses.subscribe((value: ScheduledStatusNotification[]) => {
-            console.warn(value);
-            this.scheduledStatuses = value;
+            this.scheduledStatuses.length = 0;
+
+            value.forEach(notification => {
+                notification.statuses.forEach(status => {
+                    let wrapper = new ScheduledStatusWrapper(notification.account, status);
+                    this.scheduledStatuses.push(wrapper);
+                });              
+            });
+
+            this.scheduledStatuses.sort((x, y) => new Date(x.status.scheduled_at).getTime() -new Date(y.status.scheduled_at).getTime());
         });
     }
 
     ngOnDestroy(): void {
         if (this.statusSub) this.statusSub.unsubscribe();
+    }
+}
+
+class ScheduledStatusWrapper {
+    constructor( 
+        public readonly account: AccountInfo,
+        public status: ScheduledStatus) {
     }
 }
