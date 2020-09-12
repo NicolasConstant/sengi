39af84785f67c876baac0abff83224005db49d2f
Nicolas Constant <github@nicolas-constant.com>
2020-09-12 13:03:26 -0400

    added label tutorial

--- src/app/components/tutorial-enhanced/labels-tutorial/labels-tutorial.component.ts
+++ src/app/components/tutorial-enhanced/labels-tutorial/labels-tutorial.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-labels-tutorial',
   templateUrl: './labels-tutorial.component.html',
-  styleUrls: ['../tutorial-enhanced.component.scss']
+  styleUrls: ['../tutorial-enhanced.component.scss', 'labels-tutorial.component.scss']
 })
 export class LabelsTutorialComponent implements OnInit {
 
