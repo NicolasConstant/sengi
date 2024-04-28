e6d77a9b0edd81c0ae50755cddae1dc0c3f7babd
Nicolas Constant <github@nicolas-constant.com>
2024-04-28 17:00:59 -0400

    migrating variables

--- src/app/themes/implementations/light.ts
+++ src/app/themes/implementations/light.ts
@@ -8,8 +8,9 @@ export const lightTheme: Theme = {
         "--font-color-secondary": "#bfbfbf",
         "--font-link-primary": "#595c67",
         "--font-link-primary-hover": "#8f93a2",
-        "--color-primary": "#141824",
-        "--color-secondary": "#090b10",
+
+        "--color-primary": "#ffffff",
+        "--color-secondary": "#e6ebf0",
 
         "--column-color": "#0f111a",
         "--column-header-background-color": "#0c0c10",        
@@ -18,7 +19,7 @@ export const lightTheme: Theme = {
         "--btn-primary-color": "#444f74",
         "--btn-primary-font-color": "white",
         "--status-primary-color": "#fff",
-        "--status-secondary-color": "#4e5572",
+        "--status-secondary-color": "#292c37",
         "--status-links-color": "#d9e1e8",
         "--boost-color": "#5098eb",
         "--update-color": "#95e470",
@@ -29,7 +30,7 @@ export const lightTheme: Theme = {
         "--button-color-hover": "white",
         "--button-border-color": "#303957",
 
-        "--column-background": "#0f111a",
+        "--column-background": "#f6f7f9",
 
         "--card-border-color": "#2b344d",
 
@@ -72,5 +73,35 @@ export const lightTheme: Theme = {
         "--settings-text-input-background": "#242836",
         "--settings-text-input-foreground": "white",
         "--settings-text-input-border": "#32384d",
+        
+        "--stream-column-icon": "whitesmoke",
+        "--stream-column-icon-hover": "#acacac",
+        "--stream-column-border": "#222736",
+    
+        "--label-bot": "#007281",
+        "--label-xpost": "#9c5e00",
+        "--label-thread": "#007233",
+        "--label-discuss": "#5a008f",
+        "--label-old": "#960000",
+        "--label-remote": "#264d94",
+        "--label-edited": "#5f5f5f",
+    
+        "--status-content-warning-closed-color": "#919bb1",
+        "--status-content-warning-closed-background": "#171d2b",
+    
+        "--stream-new-notification-1": "#96c0ff83",
+        "--stream-new-notification-2": "#ffffff00",
+        "--stream-new-notification-3": "#ffffff00",
+        
+        "--stream-remove-cw-background-hover": "#eeeeee",
+        "--stream-remove-cw-color-hover": "black",
+        "--stream-remove-cw-color": "white",
+    
+        "--stream-toot-error": "#ff7171",
+    
+        "--stream-toot-load-buffer-color-hover": "white",
+        "--stream-toot-load-buffer-border": "black",
+    
+        "--stream-toot-status-not-last-child-border": "#06070b",
     }
 };
