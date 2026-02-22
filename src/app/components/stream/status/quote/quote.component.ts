3d96bd593b0a5451d0fc4e68900405f4a4647b1b
Nicolas Constant <github@nicolas-constant.com>
2026-02-22 02:19:21 -0500

    added open status button on quote

--- src/app/components/stream/status/quote/quote.component.ts
+++ src/app/components/stream/status/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { Status, Account } from '../../../../services/models/mastodon.interfaces';
 import { EmojiConverter, EmojiTypeEnum } from '../../../../tools/emoji.tools';
@@ -12,6 +13,7 @@ import { SettingsService } from '../../../../services/settings.service';
 })
 export class QuoteComponent implements OnInit {
   private emojiConverter = new EmojiConverter();
+  faCircle = faCircle;
 
   private displayStatus: Status;
 
@@ -20,14 +22,14 @@ export class QuoteComponent implements OnInit {
   @Output() browseThreadEvent = new EventEmitter<OpenThreadEvent>();
 
   @Input('status')
-  set status(value: Status){
+  set status(value: Status) {
     this.displayStatus = value;
 
     if (!this.displayStatus.account.display_name) {
       this.displayStatus.account.display_name = this.displayStatus.account.username;
     }
 
-    let statusContent = this.emojiConverter.applyEmojis(this.displayStatus.emojis, this.displayStatus.content, EmojiTypeEnum.medium);
+    const statusContent = this.emojiConverter.applyEmojis(this.displayStatus.emojis, this.displayStatus.content, EmojiTypeEnum.medium);
     this.statusContent = this.ensureMentionAreDisplayed(statusContent);
 
     console.warn('statucontent');
@@ -48,15 +50,21 @@ export class QuoteComponent implements OnInit {
   ngOnInit() {
   }
 
-  //TODO: refactorise this
+  // TODO: refactorise this
   private ensureMentionAreDisplayed(data: string): string {
     const mentions = this.status.mentions;
-    if (!mentions || mentions.length === 0) return data;
+    if (!mentions || mentions.length === 0) {
+        return data;
+    }
 
     let textMentions = '';
     for (const m of mentions) {
         if (!data.includes(m.url)) {
-            textMentions += `<span class="h-card"><a class="u-url mention" data-user="${m.id}" href="${m.url}" rel="ugc">@<span>${m.username}</span></a></span> `
+            textMentions += `<span class="h-card">
+                <a class="u-url mention" data-user="${m.id}" href="${m.url}" rel="ugc">@
+                    <span>${m.username}</span>
+                </a>
+            </span> `;
         }
     }
     if (textMentions !== '') {
@@ -66,14 +74,14 @@ export class QuoteComponent implements OnInit {
   }
 
   accountSelected(accountName: string): void {
-          this.browseAccountEvent.next(accountName);
-      }
-  
-      hashtagSelected(hashtag: string): void {
-          this.browseHashtagEvent.next(hashtag);
-      }
-  
-      textSelected(): boolean {
+      this.browseAccountEvent.next(accountName);
+  }
+
+  hashtagSelected(hashtag: string): void {
+      this.browseHashtagEvent.next(hashtag);
+  }
+
+  textSelected(): boolean {
           // if (this.isSelected) return false;
   
           // const status = this._statusWrapper.status;
@@ -87,20 +95,21 @@ export class QuoteComponent implements OnInit {
           // }
   
           // this.browseThreadEvent.next(openThread);
-          return false;
-      }
 
-      getAvatar(acc: Account): string {
-          if (this.freezeAvatarEnabled) {
-              return acc.avatar_static;
-          } else {
-              return acc.avatar;
-          }
+      return false;
+  }
+
+  getAvatar(acc: Account): string {
+      if (this.freezeAvatarEnabled) {
+          return acc.avatar_static;
+      } else {
+          return acc.avatar;
       }
-  
-      openAccount(account: Account): boolean {
-          this.browseAccountEvent.emit(account.acct);
-          return false;
-      }
+  }
+
+  openAccount(account: Account): boolean {
+      this.browseAccountEvent.emit(account.acct);
+      return false;
+  }
 
 }
